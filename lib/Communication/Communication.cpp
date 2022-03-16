869793c0523c0a325fabca9c30cbca82f5665bf6
Recelis <jackymlui@live.com>
2022-03-17 09:07:36 +1000

    changing serial1 to println

--- lib/Communication/Communication.cpp
+++ lib/Communication/Communication.cpp
@@ -20,7 +20,7 @@ void Communication::sendData(String sevenDayForecast, String location)
     doc["location"] = location;
     char output[1500];
     serializeJson(doc, output);
-    Serial1.print(output);
+    Serial1.println(output);
     Serial.println(output);
 }
 
